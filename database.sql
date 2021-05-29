bdcfc9d27a1abd8d4fd1cebb8e0d93a1ce240c12
JG0nzales <jgonzales_190000001387@uic.edu.ph>
2021-05-29 22:36:50 +0800

    server to directory

---
+++ database.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE perntodo;
+
+CREATE TABLE todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255),
+  day VARCHAR(100),
+  time VARCHAR(100),
+  date date
+);
+
+-- SELECT u.user_name, t.todo_id, t.description, t.day, t.time, t.date 
+-- FROM todos AS t 
+-- LEFT JOIN users AS u 
+-- ON t.user_id = u.user_id 
+-- WHERE t.day = 'Friday'
+
+-- CREATE OR REPLACE FUNCTION public.total()
+--     RETURNS integer
+--     LANGUAGE 'plpgsql'
+--     VOLATILE
+--     PARALLEL UNSAFE
+--     COST 100
+    
+-- AS $BODY$
+-- declare
+-- total integer;
+-- begin
+-- select count(*) into total from film;
+-- return total;
+-- end;
+-- $BODY$;
